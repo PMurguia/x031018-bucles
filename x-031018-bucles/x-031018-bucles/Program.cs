c3a85d3a86119bd1d619def20715b67b112ca9d7
Peio Murguia <peiomurguia@gmail.com>
2018-10-03 17:02:44 +0200

    Primera Subida

---
+++ x-031018-bucles/x-031018-bucles/Program.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace x_031018_bucles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escribe un programa que muestre en pantalla los números del 10 al 20(incluidos los dos) con una línea vacía entre ellos.
+
+            //int numSuma = 10;
+            //do
+            //{
+            //    Console.WriteLine(numSuma);
+            //    Console.WriteLine();
+            //    numSuma++;
+            //} while (numSuma <= 20);
+            //Console.ReadLine();
+            //Console.WriteLine();
+
+
+            ////Escribe un programa que muestre en pantalla los números del 20 al 10 (incluidos los dos) con una línea vacía entre ellos.
+
+            //int numResta = 20;
+            //do
+            //{
+            //    Console.WriteLine(numResta);
+            //    Console.WriteLine();
+            //    numResta--;
+            //} while (numResta >= 10);
+            //Console.ReadLine();
+            //Console.WriteLine();
+
+
+            ////Escribe un programa que pida 10 valores. Al final mostrar la media de esos valores
+
+            //int numMedia;
+            //int sumaNumMedia = 0;
+
+            //for(int i=0; i <10; i++)
+            //{
+            //    Console.WriteLine("Dame un número. ");
+            //    numMedia = Int32.Parse(Console.ReadLine());
+            //    sumaNumMedia = numMedia + sumaNumMedia;
+            //}
+            //Console.WriteLine("Media: " + sumaNumMedia / 10);
+            //Console.ReadLine();
+
+            ////Escribe un programa que pida valores hasta que el usuario introduzca un 0. Si introduce un valor negativo,
+            ////mostrar un mensaje diciendo que se ignoran los valores negativos. Mostrar la suma de los valores introducidos.
+
+            //int num1;
+            //int numSuma1 = 0;
+
+            //do
+            //{
+            //    Console.WriteLine("Introduce un número. ");
+            //    num1 = Int32.Parse(Console.ReadLine());
+
+            //    if (num1 < 0)
+            //    {
+            //        Console.WriteLine("No se aceptan números negativos. ");
+            //    }
+            //    else
+            //    {
+            //        numSuma1 = num1 + numSuma1;
+
+            //    }
+
+            //}
+            //while (num1 != 0);
+            //Console.WriteLine("Total: " + numSuma1);
+            //Console.ReadLine();
+
+
+
+            //Escribe un programa que muestre todos los valores pares o impares entre 1 y 100 
+            //dependiendo de si el usuario introduce P o I.
+            //Si introduce otra cosa, decir que ese valor no es correcto y pedirle otra vez.
+
+            //string respuesta = "";
+
+
+            //while(respuesta != "P" && respuesta != "I")
+            //{
+            //    Console.WriteLine("Escribe P o I. ");
+            //    respuesta = Console.ReadLine();
+            //}
+
+            //if(respuesta=="P")
+            //{
+            //    for(int i = 2 ; i < 101 ; i = i + 2 )
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            //else
+            //{
+            //    for(int i = 1 ; i < 101 ; i = i + 2 )
+            //        {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            //Console.ReadLine();
+
+
+            // Escribe un programa que pregunte al usuario su edad.Mostrar en pantalla “Feliz cumpleaños” repetido tantas veces como años tenga.
+
+            //int edad;
+
+            //do
+            //{
+            //    Console.WriteLine("¿Cuántos años tienes?");
+            //    edad = Int32.Parse(Console.ReadLine());
+            //}
+            //while (edad < 0);
+
+            //for (int i = 1; i <= edad; i++)
+            //{
+            //    Console.WriteLine("FELIZ CUMPLEAÑOS. ");
+            //}
+            //Console.ReadLine();
+            //Console.WriteLine();
+
+
+            // //Pedir al usuario un número.Mostrar en pantalla
+            // //*
+            // //**
+            // //***
+            // //Con tantas líneas como el número que ha introducido
+
+            // int respuesta;
+
+
+            // Console.WriteLine("Dime un número. ");
+            // respuesta = Int32.Parse(Console.ReadLine());
+
+
+            // for (int i = 0; i < respuesta; i++)
+            // {
+            //     for(int j = 0; j <= i; j++)
+            //     {
+            //         Console.Write("*");
+            //     }
+            //     Console.WriteLine();
+            // }
+            // Console.ReadLine();
+
+
+            //Escribe un programa que muestre los números de 1 a 12 en una columna. 
+            //En la columna de al lado de los números impares mostrar el cuadrado de ese valor
+            //y en la de los pares mostrar el cubo.
+
+
+
+
+            //for(int i=1; i<=12; i++)
+            //{
+            //        Console.Write(i + "\t");
+
+
+            //        if (i % 2 == 0)
+            //        {
+            //            Console.Write(Math.Pow(i, 3));
+            //        }
+            //        else 
+            //        {
+            //            Console.Write(Math.Pow(i, 2));
+            //        }
+            //    Console.WriteLine();
+            //    }
+
+            //    Console.ReadLine();
+            //    Console.WriteLine();
+            //}
+
+            //Mostrar en pantalla diez números, siendo el primero 1 y los siguientes la suma del número anterior y 5.
+
+            //int suma = 1;
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(suma);
+            //    suma = suma + 5;
+            //}
+            //Console.ReadLine();
+
+            //Crear un programa que muestre el valor de sumar todos los números desde 1 hasta el número que introduzca el usuario.
+            //Por ejemplo si introduce el 8 debería mostrar 36(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+           
+
+
+
+            }
+
+
+
+
+
+        }
+      
+    }
+}
+                
+            
+        
+        
+    
+
+            
+
+                
+            
+
+
+
+           
+           
+            
+           
